4da322832ae987df5f4861ec37d6ccc6ebeb5902
scottqueen-bixal <scott.queen@bixal.com>
2025-05-28 13:14:51 -0400

    feat: add CORS middleware to FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.internal import admin
 from backend.routers import Todos  # Import the todos router
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5173",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include our admin router
 app.include_router(admin.router)
