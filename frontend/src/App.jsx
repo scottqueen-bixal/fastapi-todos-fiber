a3bc6a1d6aeda8a701188ca87c4216ca41f47a12
scottqueen-bixal <scott.queen@bixal.com>
2025-05-29 13:10:46 -0400

    feat: implement update and delete functionality for todos, enhance fetching logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Todo from "./components/Todo";
 import "./App.css";
 
 const todosPath = "http://localhost:8000/todos";
@@ -20,8 +21,13 @@ function App() {
       }
 
       const data = await response.json();
-      setTodos((prevTodos) => [...prevTodos, ...data]);
-      console.info("Todos fetched successfully:", data);
+      // Sort todos by created_at in descending order
+      const sortedTodos = data.sort(
+        (a, b) => new Date(a.created_at) - new Date(b.created_at)
+      );
+      console.log({ sortedTodos });
+      setTodos(sortedTodos);
+      console.info("Todos fetched successfully:", sortedTodos);
     } catch (error) {
       console.error("Error fetching todos:", error);
     }
@@ -44,10 +50,55 @@ function App() {
       }
 
       const todo = await response.json();
-      setTodos((prevTodos) => [...prevTodos, todo]);
+      console.log(todo);
+      setTodos(getTodos());
       console.info("Todo created successfully:", todo);
     } catch (error) {
       console.error("Error creating todo:", error);
+    }
+  };
+
+  const updateTodo = async ({ content, id, is_completed }) => {
+    const newTodo = { content, id, is_completed };
+
+    try {
+      const response = await fetch(`${todosPath}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update todo");
+      }
+
+      const updatedTodo = await response.json();
+      setTodos(getTodos());
+      console.info("Todo updated successfully:", updatedTodo);
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  };
+
+  const deleteTodo = async ({ id }) => {
+    try {
+      const response = await fetch(`${todosPath}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete todo");
+      }
+
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      console.info("Todo deleted successfully:", id);
+    } catch (error) {
+      console.error("Error deleting todo:", error);
     }
   };
 
@@ -58,13 +109,39 @@ function App() {
 
   return (
     <div className="todo-app">
-      <button onClick={() => createTodo("TEST Todo")}>Create Todo</button>
-      {todos.length > 0 &&
-        todos.map((todo, index) => (
-          <div key={index}>
-            <p>{todo.task}</p>
+      <button className="create-todo" onClick={() => createTodo("TEST Todo")}>
+        Create Todo
+      </button>
+      {todos.length > 0 && (
+        <div className="todos">
+          <div className="todo-list">
+            {todos.map(
+              (todo, index) =>
+                !todo.is_completed && (
+                  <Todo
+                    key={index}
+                    todo={todo}
+                    onToggle={updateTodo}
+                    onDelete={deleteTodo}
+                  />
+                )
+            )}
           </div>
-        ))}
+          <div className="todo-list">
+            {todos.map(
+              (todo, index) =>
+                todo.is_completed && (
+                  <Todo
+                    key={index}
+                    todo={todo}
+                    onToggle={updateTodo}
+                    onDelete={deleteTodo}
+                  />
+                )
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
