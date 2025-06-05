373cfc7306a4a9f839eab5e1a7072928e2d7ce63
scottqueen-bixal <scott.queen@bixal.com>
2025-06-05 11:06:16 -0400

    feat: implement virtualized rendering for TodoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef, useMemo, lazy, Suspense } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import { getTodos, createTodo } from "./utils";
 import "./App.css";
 
@@ -7,8 +8,16 @@ const TodoList = lazy(() => import("./components/TodoList"));
 function App() {
   const [todos, setTodos] = useState(() => []);
   const inputTask = useRef();
+  const parentRef = useRef(null);
 
   const memoizedTodos = useMemo(() => todos, [todos]);
+
+  // virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: memoizedTodos.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+  });
 
   const handleCreateTodo = (e) => {
     e.preventDefault();
@@ -22,14 +31,30 @@ function App() {
   }, []);
 
   return (
-    <div className="todo-app">
+    <div
+      className="todo-app"
+      ref={parentRef}
+      style={{ height: "100vh", overflow: "auto" }}
+    >
       <form className="todo-form" onSubmit={handleCreateTodo}>
         <input ref={inputTask} />
         <button type="submit">Create Todo</button>
       </form>
-      <Suspense fallback={<div>Loading Todos...</div>}>
-        <TodoList todos={memoizedTodos} setTodos={setTodos} />
-      </Suspense>
+      <div
+        style={{
+          height: `${rowVirtualizer.getTotalSize()}px`,
+          width: "100%",
+          position: "relative",
+        }}
+      >
+        <Suspense fallback={<div>Loading Todos...</div>}>
+          <TodoList
+            todos={memoizedTodos}
+            setTodos={setTodos}
+            rowVirtualizer={rowVirtualizer}
+          />
+        </Suspense>
+      </div>
     </div>
   );
 }
