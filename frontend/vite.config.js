0d828680a5d4063254418d94a9865beed38ed7b7
scottqueen-bixal <scott.queen@bixal.com>
2025-06-05 07:27:20 -0400

    feat: load environment variables in Vite config for dynamic server allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_BASE_URL || "127.0.0.1", "localhost"],
+    },
+  };
+});
