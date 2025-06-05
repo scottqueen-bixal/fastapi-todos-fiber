071219e720fa6e919289f789dcfbc9cb3c11c8c8
scottqueen-bixal <scott.queen@bixal.com>
2025-06-05 12:40:29 -0400

    refactor: clean up TodoList component structure for improved readability

--- frontend/src/components/TodoList/index.jsx
+++ frontend/src/components/TodoList/index.jsx
@@ -7,29 +7,27 @@ const TodoList = ({ todos, setTodos, rowVirtualizer }) => {
     /* Only the visible items in the virtualizer, manually positioned to be in view */
   }
   return (
-    <div className="todos">
-      <div className="todo-list">
-        {rowVirtualizer.getVirtualItems().map((virtualItem) => {
-          const item = todos[virtualItem.index];
-          return (
-            <div
-              className="virtual-item"
-              key={`todo-${virtualItem.key}`}
-              style={{
-                "--virtual-item-height": `${virtualItem.size}px`,
-                "--virtual-item-translate-y": `${virtualItem.start}px`,
-              }}
-            >
-              <Todo
-                key={`todo-${item.id}`}
-                todo={item}
-                onToggle={(item) => updateTodo(item, setTodos)}
-                onDelete={() => deleteTodo(item.id, setTodos)}
-              />
-            </div>
-          );
-        })}
-      </div>
+    <div className="todo-list">
+      {rowVirtualizer.getVirtualItems().map((virtualItem) => {
+        const item = todos[virtualItem.index];
+        return (
+          <div
+            className="virtual-item"
+            key={`todo-${virtualItem.key}`}
+            style={{
+              "--virtual-item-height": `${virtualItem.size}px`,
+              "--virtual-item-translate-y": `${virtualItem.start}px`,
+            }}
+          >
+            <Todo
+              key={`todo-${item.id}`}
+              todo={item}
+              onToggle={(item) => updateTodo(item, setTodos)}
+              onDelete={() => deleteTodo(item.id, setTodos)}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
